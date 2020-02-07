8b9610e5a0feca9d6a1d6fc878e42f15c057242d
faint <bunnyfaint@me.com>
2020-02-07 10:15:23 +0800

    新增战斗角色类

---
+++ assets/resources/script/Actor.ts
@@ -0,0 +1,30 @@
+export default class Actor {
+    opponent: Actor = null;
+    
+
+    chaseStart() {
+        // 负面状态
+        // 正面状态
+    }
+
+    chaseEnd() {
+        // 后置祈祷
+    }
+
+    drawCard() {
+
+    }
+
+    playCard() {
+
+    }
+
+    Hit() {
+        cc.log("hit");
+    }
+
+    BeHit() {
+
+    }
+
+}
